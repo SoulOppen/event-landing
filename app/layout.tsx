ebeead252fbb74ef70258fffe0afedf454644229
souloppen <arieloppenheimer@outlook.cl>
2026-02-27 12:35:03 -0300

    add:smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
 
       <body className="min-h-screen flex flex-col justify-between antialiased ">
       <div className="relative w-full h-64">
